0622edd6925fd0021e19d5f6bbaf86c87c6d9062
Vicente Bruno Nonato Paranhos <vicentebrunonp@outlook.com>
2022-03-22 23:44:02 -0300

    :idea: Add Project

---
+++ src/juros.api/Services/ServiceTaxaJuros.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using RestSharp;
+using System;
+
+namespace juros.api.Services
+{
+    public static class ServiceTaxaJuros
+    {
+        public static double BuscaTaxaJuros()
+        {
+            double resp;
+
+            try
+            {
+                var client = new RestClient($"{Environment.GetEnvironmentVariable("APITAXAS")}/taxaJuros");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.GET);
+                IRestResponse response = client.Execute(request);
+
+                resp = Convert.ToDouble(response.Content.Replace(".", ","));
+            }
+            catch (Exception)
+            {
+                throw new Exception();
+            }
+
+            return resp;
+
+        }
+    }
+}
